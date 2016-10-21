bfd6adf1c40909617701b2078050456204897b15
qingyuan <ae5656ln@163.com>
2016-10-21 22:55:56 +0800

    Bidirectional LSTM

--- test.py
+++ test.py
@@ -2,6 +2,5 @@ __author__ = 'PC-LiNing'
 
 import numpy as np
 
-# sent_matrix=np.ndarray(shape=(5,5),dtype=np.float32)
-sent_matrix = np.zeros(shape=(5,5),dtype=np.float32)
-print(sent_matrix)
+test = 1.5e-3
+print(test)
